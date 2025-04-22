7e3630b0ebc460fce8426962d2aa4040af4cfabd
berkmahim <berkmahim620@gmail.com>
2025-04-22 17:01:09 +0300

    cors

--- satis-takip/backend/server.js
+++ satis-takip/backend/server.js
@@ -34,7 +34,10 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://playful-empanada-30e99b.netlify.app',
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
